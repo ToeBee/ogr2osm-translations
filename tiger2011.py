f9d0a404bee9236887de786c225959690ece2d19
Toby Murray <toby.murray@gmail.com>
2012-03-10 20:45:34 -0600

    Made functional for a small area of remapping in Glendale, CA

--- tiger2011.py
+++ tiger2011.py
@@ -1,8 +1,24 @@
 '''
 ogr2osm translation file for converting TIGER 2011 shapefiles into .osm format.
 
+NOTE! To use this translation, you need to download the "featnames" file that ccompanies each TIGER shapefile. Then you need to join 
+that file to the shapefile in QGIS or ArcMap or something based on LINEARID. This will allow for the street name abbreviations to 
+be processed correctly. 
+
 This translation file requires the new version of ogr2osm (see readme)
 '''
+
+'''import dbf
+
+class dbfReader:
+    index = None
+'''
+    
+class lookups:
+    directions = {'N':'North', 'E':'East', 'S':'South', 'W':'West', 'NE':'Northeast', 'NW':'Northwest', 'SE':'Southeast', 'SW':'Southwest'}
+    streetTypes = {'Ave':'Avenue', 'Cam':'Camino', 'Way':'Way', 'Walk':'Walk', 'Ter':'Terrace', 
+                   'St':'Street', 'Rd':'Road', 'Pl':'Place', 'Ln':'Lane', 'Fwy':'Freeway', 
+                   'Dr':'Drive', 'Ct':'Court', 'Cir':'Circle', 'Blvd':'Boulevard', 'Aly':'Alley'}
 
 def filterTags(attrs):
     if not attrs: return
@@ -13,8 +29,21 @@ def filterTags(attrs):
     tags.update({'tiger:linearid':attrs['LINEARID']})
     tags.update({'tiger:source':'TIGER 2011 ogr2osm import'})
     tags.update({'tiger:reviewed':'no'})
+    
+    #commented out code is an attempt to read the TIGER feature names in for processing names. For now I'm joining this to the shapefile in QGIS.
+    #This may not be ideal for roads with multiple names but right now I'm just doing one section of residential roads so it should work ok. 
+    '''if(dbfReader.index == None): 
+        table = dbf.Table('featnames.dbf')
+        print 'indexing feature names'
+        dbfReader.index = table.create_index(lambda rec: rec.linearid)
+        print 'done indexing'
+        
+    records = dbfReader.index.search(match=(attrs['LINEARID']))
+    print 'found records for ' + attrs['LINEARID']
+    print records '''
+    
     if attrs['FULLNAME']:
-        tags.update({'name':attrs['FULLNAME']})
+        tags.update({'name':composeName(attrs)})
     
     tags.update({'highway':mtfccToHighway(attrs['MTFCC'])})
     
@@ -31,6 +60,36 @@ def filterTags(attrs):
     
     return tags
 
+def composeName(attrs):
+    print 'composing name'
+    finalName = ''
+    if(attrs['PREDIRABRV']):
+        finalName += safeLookup(attrs['PREDIRABRV'], lookups.directions) + ' '
+    if(attrs['PRETYPABRV']):
+        finalName += safeLookup(attrs['PRETYPABRV'], lookups.streetTypes) + ' '
+    if(attrs['NAME']):
+        finalName += attrs['NAME'] + ' '
+    if(attrs['SUFTYPABRV']):
+        finalName += lookups.streetTypes[attrs['SUFTYPABRV']] + ' '
+    print 'returning name'
+    return finalName.rstrip()
+
+def safeLookup(key, lookup):
+    try:
+        return lookup[key]
+    except KeyError:
+        print 'key not found: ' + key
+        return '';
+
+def translateDirection(abbrev):
+    if abbrev == 'E':
+        return 'East'
+    elif abbrev == 'S':
+        return 'South'
+    elif abbrev == 'W':
+        return 'West'
+    elif abbrev == 'N':
+        return 'North'
 
 def mtfccToHighway(mtfcc):
     if mtfcc == 'S1100':
